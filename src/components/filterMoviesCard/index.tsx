827cb6481bdf21d5c101319e38340f6ad52d875a
Prratiiik <144805450+Prratiiik@users.noreply.github.com>
2024-04-19 16:06:23 +0100

    Added filtering of movie list.

--- src/components/filterMoviesCard/index.tsx
+++ src/components/filterMoviesCard/index.tsx
@@ -27,6 +27,7 @@ const styles = {
 };
 
 interface FilterMoviesCardProps {
+    onUserInput: (f: FilterOption, s: string)  => void; // Add this line
     titleFilter: string;
     genreFilter: string;
   }
@@ -48,9 +49,9 @@ const FilterMoviesCard: React.FC<FilterMoviesCardProps> = (props) => {
      // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
  
-  const handleChange = (e: SelectChangeEvent, type: FilterOption, value: string) => {
+   const handleChange = (e: SelectChangeEvent, type: FilterOption, value: string) => {
     e.preventDefault()
-    // Completed later
+    props.onUserInput(type, value)
   };
 
   const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
